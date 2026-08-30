4d5f94824edcae8cfe6ac8e749bc2d8785ac3d59
DevR-Hub2410 <r.castillov4@gmail.com>
2026-08-30 17:19:07 -0600

    Se agrego algunas definiciones y especificaciones de varios metodos y se edito el initLesson en la leccion 2

--- Muestreo_Unidad_II/02_Metodos_de_Muestreo/initLesson.R
+++ Muestreo_Unidad_II/02_Metodos_de_Muestreo/initLesson.R
@@ -2,3 +2,18 @@
       # lesson is started. Any variables created here will show up in
       # the user's working directory and thus be accessible to them
       # throughout the lesson.
+
+set.seed(123)
+
+poblacion <- data.frame(
+  id = 1:500,
+  edad = sample(18:25, 500, replace = TRUE),
+  carrera = sample(
+    c("Ingeniería", "Administración", "Derecho", "Medicina"),
+    500,
+    replace = TRUE
+  ),
+  semestre = sample(1:10, 500, replace = TRUE)
+)
+
+n <- 50
