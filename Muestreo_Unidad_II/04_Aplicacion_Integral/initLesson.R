d530c51fa7b9f07068fe509e86587bba62889bd8
Zyanya Mireles <a.22308051281080@cbtis128.edu.mx>
2026-09-07 21:05:58 -0600

    Se agrega referencia la leccion 3 y se completa leccion 4 con actividad integradora, probada exitosamente

--- Muestreo_Unidad_II/04_Aplicacion_Integral/initLesson.R
+++ Muestreo_Unidad_II/04_Aplicacion_Integral/initLesson.R
@@ -1,4 +1,8 @@
-# Code placed in this file fill be executed every time the
-      # lesson is started. Any variables created here will show up in
-      # the user's working directory and thus be accessible to them
-      # throughout the lesson.
+set.seed(123)
+
+usuarios_app <- data.frame(
+  id = 1:800,
+  region = sample(c("Norte", "Centro", "Sur"), 800, replace = TRUE),
+  plan = sample(c("Gratuito", "Premium"), 800, replace = TRUE, prob = c(0.7, 0.3)),
+  satisfaccion = round(rnorm(800, mean = 7, sd = 1.5), 1)
+)
